bd7c6eafd4b4ff533f90edc06c5af62352d8545e
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2019-03-25 14:06:41 +0000

    Update period

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Matt Timmons-Brown", "a programmer.", "a computer science student.", "an electronics student.", "a YouTuber.", "an author."],
+      strings: ["Matt Timmons-Brown.", "a programmer.", "a computer science student.", "an electronics student.", "a YouTuber.", "an author."],
       typeSpeed: 100,
       loop: true,
     });
